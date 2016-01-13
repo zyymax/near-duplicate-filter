6518472ac0ed46b5f5087e9518500ac849848684
木沉 <muchen.zyy@alibaba-inc.com>
2016-01-14 00:59:04 +0800

    work now~~~

--- filter_duplicate.sh
+++ filter_duplicate.sh
@@ -10,30 +10,25 @@ prefix=`echo $1 | awk '{split($0, arr, "/");print arr[2];}' | awk -F '.' '{print
 full_prefix=$dir_path/$prefix
 
 #过滤字符，提取有效文本
-sh webcontent_filter.sh $1 "$full_prefix.ori"
+#./webcontent_filter.sh $1 "$full_prefix.ori"
+cp $1 "$full_prefix.ori"
 cat "$full_prefix.ori" | awk '{if(length($0)>60){print NR"\t"$0}}' > "$full_prefix.rowori"
-rm -f "$full_prefix.ori"
+#rm -f "$full_prefix.ori"
 
 #分词
-python python/tokens.py '-s'  "$full_prefix.rowori" "$full_prefix.token" 'c' 'data/stopwords.txt'
+python/tokens.py '-s'  "$full_prefix.rowori" "$full_prefix.token" 'c' 'data/stopwords.txt'
 rm -f "$full_prefix.rowori"
 
 #生成特征词典
-python python/DictBuilder.py "$full_prefix.token" "$full_prefix.dict"
+python/DictBuilder.py "$full_prefix.token" "$full_prefix.dict"
 
 #用hadoop streaming生成Simhash指纹
 cp "$full_prefix.dict" 'word.dict'
-hadoop fs -rm "/test/max.zhang/sample_data/$prefix.token"
-hadoop fs -put "$full_prefix.token" '/test/max.zhang/sample_data'
-hadoop fs -rmr "/test/max.zhang/sample_data/"$prefix"_rowfprint"
-hadoop jar /home/app/srv/hadoop/contrib/streaming/hadoop-streaming-1.2.1.jar -mapper python/SimhashMapper.py -file python/SimhashMapper.py -input "/test/max.zhang/sample_data/$prefix.token" -output "/test/max.zhang/sample_data/"$prefix"_rowfprint" -file 'word.dict'
-rm -f 'word.dict'
-rm -rf "$full_prefix"_rowfprint
-hadoop fs -get "/test/max.zhang/sample_data/"$prefix"_rowfprint" $dir_path
+cat "$full_prefix.token" | python/SimhashMapper.py > "$full_prefix.rowfprint"
 
-#由.token和rowfprint/part-00000生成.lenfprint
+#由.token和.rowfprint生成.lenfprint
 sort -k1 -n "$full_prefix.token" > tmp_1
-sort -k1 -n ""$full_prefix"_rowfprint/part-00000" > tmp_2
+sort -k1 -n "$full_prefix.rowfprint" > tmp_2
 paste tmp_1 tmp_2 |
 awk -F '\t' '{print $1"\t"length($2)"\t"$4}' > $full_prefix.lenfprint
 rm -f tmp_1
@@ -41,3 +36,5 @@ rm -f tmp_2
 #自去重
 python python/FilterSalientBySimhashNoScore.py $full_prefix.lenfprint \
      $full_prefix.lenfprint "$full_prefix"_uniq.lenfprint
+#去重后文档集合
+python/get_row_sample.py $1 "$full_prefix"_uniq.lenfprint > "$full_prefix"_uniq.content
